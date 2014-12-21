c30a85bfab0346cdebc91c283a4e30d0e421a9eb
Nate Brune <natebrune@gmail.com>
2014-12-20 21:59:29 -0500

    added comments to reboot.s

--- reboot.s
+++ reboot.s
@@ -1,8 +1,11 @@
 .globl	main
-main:
-  movq $169,%rax
-  movq $0xfee1dead,%rdi
-  movq $85072278,%rsi
-  movq $0x01234567,%rdx
-  syscall
-  ret
+# For a quick intro to syscalls, look at helloworld.s
+main: 
+  movq $169,%rax        # 169 is the numerical representation of reboot() as defined in unistd_64.h
+  movq $0xfee1dead,%rdi # Magic1; these bytese are a kernel easter egg, discover the rainbow at
+  movq $85072278,%rsi   # Magic2A; http://man7.org/linux/man-pages/man2/reboot.2.html
+  movq $0x01234567,%rdx # Numerical representation of the command LINUX_REBOOT_CMD_RESTART as defined in reboot.h
+  syscall               # Execute the syscall like it 1999
+  ret                   # TBH, we just executed the syscall, what is this returning too? 
+                        # This lone program, in the vast emptyness of a rebooting system
+                        # Makes you think.
